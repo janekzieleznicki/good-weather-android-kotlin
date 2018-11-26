3e60b023cd1fc4afb1d13262fd06466a82e4d4dc
Janek <janek.zieleznicki@gmail.com>
2018-11-26 16:45:16 +0100

    weather provider

--- app/src/main/java/pl/training/goodweather/model/WeatherProvider.kt
+++ app/src/main/java/pl/training/goodweather/model/WeatherProvider.kt
@@ -9,4 +9,5 @@ interface WeatherProvider {
     @GET("forecast/daily?cnt=10&units=metric&appid=b933866e6489f58987b2898c89f542b8")
     fun getForecast(@Query("q") place: String): Observable<ForecastResultTo>
 
+
 }
